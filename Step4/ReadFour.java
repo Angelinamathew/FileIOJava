f1353f03a660956b1a965526c628710150dfce97
Angelinamathew <angelinamathew707@gmail.com>
2024-03-12 11:47:38 -0400

    Finished

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,5 +1,6 @@
 package Step4;
 
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.io.File;
@@ -16,7 +17,7 @@ public class ReadFour
             try{
                 System.out.print("Writing to file ... ");
                 // Create the file to write to
-                PrintWriter fileOut = new PrintWriter("/Users/angelina/Desktop/Projects/FileIOJava/Step4/input.txt");
+                PrintWriter fileOut = new PrintWriter("output.txt");
                 // Write text just like we would to the console
                 fileOut.println("Hello ");
                 // Clode out file
@@ -25,7 +26,7 @@ public class ReadFour
                 System.out.println("Done!");
 
                 // Read in file to verify it
-                Scanner fileIn = new Scanner(new File("/Users/angelina/Desktop/Projects/FileIOJava/Step4/input.txt"));
+                Scanner fileIn = new Scanner(new File("/Users/angelina/Desktop/Projects/FileIOJava/Step4/output.txt"));
 
                 System.out.println("Reading from file ... ");
                 while (fileIn.hasNext())
@@ -35,8 +36,28 @@ public class ReadFour
                 }
                 System.out.println("Done!");
             }
+            catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Scanner fileIn = new Scanner(new File("/Users/angelina/Desktop/Projects/FileIOJava/Step4/input.txt"));
+                int sum = 0;
+                while (fileIn.hasNext())
+                {
+                    // Reads the entire line as a string
+                    String lineIn = fileIn.nextLine();
+                    // Split the line into a String array
+                    String[] input = lineIn.split(",");
+
+                    for (int i = 0; i < input.length; i ++) {
+                        sum += Integer.parseInt(input[i]);
+                    }
+                    System.out.println(sum);
+                }
+            }
             catch (IOException e) {
                 System.out.println("File not found");
             }
         }
+
 }
